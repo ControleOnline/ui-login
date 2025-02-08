597f55fe020d42013c3c451b9b88ba68bd203fd9
Luiz Kim <luiz.kim@controleonline.com>
2025-02-08 13:50:07 -0300

    Update

--- src/store/modules/auth/mutations.js
+++ src/store/modules/auth/mutations.js
@@ -2,9 +2,7 @@ import { LocalStorage } from "quasar";
 import * as types from "./mutation_types";
 
 export default {
-  [types.LOGIN_SET_USER](state, payload) {
-    let user = payload?.user || payload;
-
+  [types.LOGIN_SET_USER](state, user) {
     if (!user) {
       LocalStorage.remove("session");
       state.isLoggedIn = false;
@@ -12,32 +10,27 @@ export default {
       LocalStorage.set("session", user);
       state.isLoggedIn = true;
     }
-    if (!payload?.user) Object.assign(state, { user });
-    return { ...state, user: user };
+    state.user = user;
   },
 
-  [types.SET_PEOPLE_STATUS](state, payload) {
-    if (!payload?.peopleStatus) Object.assign(state, { peopleStatus: payload });
-    return { ...state, isLoading: payload.peopleStatus || payload };
+  [types.SET_PEOPLE_STATUS](state, peopleStatus) {
+    state.peopleStatus = peopleStatus;
+    state.isLoading = peopleStatus || false;
   },
 
-  [types.LOGIN_SET_ERROR](state, payload) {
-    if (!payload?.error) Object.assign(state, { payload });
-    return { ...state, error: payload.error || payload };
+  [types.LOGIN_SET_ERROR](state, error) {
+    state.error = error;
   },
 
-  [types.LOGIN_SET_ISLOADING](state, payload = true) {
-    if (!payload?.isLoading) Object.assign(state, { payload });
-    return { ...state, isLoading: payload.isLoading || payload };
+  [types.LOGIN_SET_ISLOADING](state, isLoading = true) {
+    state.isLoading = isLoading;
   },
 
-  [types.LOGIN_SET_VIOLATIONS](state, payload) {
-    if (!payload?.violations) Object.assign(state, { violations: payload });
-    return { ...state, violations: payload.violations || payload };
+  [types.LOGIN_SET_VIOLATIONS](state, violations) {
+    state.violations = violations;
   },
 
-  [types.LOGIN_SET_INDEX_ROUTE](state, payload) {
-    if (!payload?.indexRoute) Object.assign(state, { indexRoute: payload });
-    return { ...state, indexRoute: state.indexRoute || payload };
+  [types.LOGIN_SET_INDEX_ROUTE](state, indexRoute) {
+    state.indexRoute = indexRoute;
   },
 };
