5b874ea6736d2bc421d1b86e3ba6a40534e86523
Luiz Kim <luiz.kim@controleonline.com>
2025-03-12 16:42:30 -0300

    Update

---
+++ src/react/router/routes.js
@@ -0,0 +1,11 @@
+import SignInPage from "@controleonline/ui-login/src/react/pages/sign-in/index";
+
+const loginRoutes = [
+  {
+    name: "SignInPage",
+    component: SignInPage,
+    options: { headerShown: false },
+  },
+];
+
+export default loginRoutes;
