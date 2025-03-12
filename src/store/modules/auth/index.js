a822379a550a41bab502048526c2fae7d888c0bb
Luiz Kim <luiz.kim@controleonline.com>
2025-03-12 12:04:08 -0300

    Update

--- src/store/modules/auth/index.js
+++ src/store/modules/auth/index.js
@@ -5,7 +5,7 @@ import mutations from './mutations';
 export default {
   namespaced: true,
   state: {
-    user: JSON.parse(localStorage.getItem('session')) || {},
+    user: localStorage.getItem('session'),
     isLoading: false,
     isLoggedIn: false,
     error: '',
